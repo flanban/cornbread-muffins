57b33271bc42a1bf440aa9fd0f6323adff387145
flanban <johnsfuller@gmail.com>
2015-07-19 23:09:51 -0400

    link to correct slide

--- app/assets/javascripts/slider.js
+++ app/assets/javascripts/slider.js
@@ -1,31 +1,40 @@
-function initSlider() {
-  var si = $('.royalSlider').royalSlider({
-    controlNavigation:'none',
-    imageScaleMode:'none',
-    imageScalePadding:6,
-    imageAlignCenter:false,
-    slidesSpacing:0,
-    globalCaption:true,
-    arrowNav:false,
-    loop:false,
-    usePreloader: true  
-  })  
-}
 
-$( document ).ready(function() {
-  initSlider();
+
+$( document ).ready(function(e) {
+
   $('body').on('click', '.slider-link', function (e){
     e.preventDefault();
     $('.slider-nav').fadeIn('slow');
     $('.royalSlider, #slider-wrapper').fadeIn();
-//    var slideNum = $(this).index()
+    slideNum = $(this).index();
+    console.log(slideNum)
   });
+  
 
+  
   $('body').on('click', '.button-close', function (e){
       e.preventDefault();
       //$('.image-slider').royalSlider('destroy').empty();
       $('.slider-nav, .image-slider, #slider-wrapper').fadeOut('slow');
-  });  
+  });    
 });
 
+  
+  function initSlider() {
+    var si = $('.royalSlider').royalSlider({
+      controlNavigation:'none',
+      imageScaleMode:'none',
+      imageScalePadding:6,
+      imageAlignCenter:false,
+      slidesSpacing:0,
+      globalCaption:true,
+      arrowNav:false,
+      loop:false,
+      usePreloader: true,
+      startSlideId:slideNum, 
+    }).data('royalSlider');
+  }
+  
+  
+  initSlider();
 
